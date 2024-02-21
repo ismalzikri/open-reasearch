3b89176113dcdd7bfa4016794a521b3c4d479912
Ismal zikri <zikriismal42@gmail.com>
2024-02-21 09:08:38 +0700

    feat(src/data, src/utils): add list colors & algorithm for color detection

---
+++ src/utils/index.ts
@@ -0,0 +1,41 @@
+import { knownColors } from "../data/colors";
+
+export function rgbToHex(r: number, g: number, b: number): string {
+  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+export function getColorName(hexCode: string): string {
+  const userColor = hexToRGB(hexCode.toLowerCase());
+
+  let closestColor = null;
+  let minDistance = Infinity;
+
+  for (const color of knownColors) {
+    const knownColor = hexToRGB(color.code.toLowerCase());
+    const distance = calculateColorDistance(userColor, knownColor);
+
+    if (distance < minDistance) {
+      minDistance = distance;
+      closestColor = color.name;
+    }
+  }
+
+  return closestColor || "Unknown";
+}
+
+function hexToRGB(hex: string): { r: number; g: number; b: number } {
+  const r = parseInt(hex.substring(1, 3), 16);
+  const g = parseInt(hex.substring(3, 5), 16);
+  const b = parseInt(hex.substring(5, 7), 16);
+  return { r, g, b };
+}
+
+function calculateColorDistance(
+  color1: { r: number; g: number; b: number },
+  color2: { r: number; g: number; b: number }
+): number {
+  const deltaR = color1.r - color2.r;
+  const deltaG = color1.g - color2.g;
+  const deltaB = color1.b - color2.b;
+  return Math.sqrt(deltaR * deltaR + deltaG * deltaG + deltaB * deltaB);
+}
