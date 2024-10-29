8a047d260977814cca82911a4dde7f7a9d59a705
Ismal zikri <zikriismal42@gmail.com>
2024-10-29 15:51:53 +0700

    add new url at next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
   reactStrictMode: true,
   env: {
     URL_TRANSLATION: process.env.URL_TRANSLATION,
+    URL_TEXTTOSPEECH: process.env.URL_TEXTTOSPEECH,
   },
   images: {
     domains: ["color-assistant.vercel.app"],
